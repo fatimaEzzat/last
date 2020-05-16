e033d6cd6b40c0257c3ed550306f26b5a2c155c5
doragrestyaa@gmail.com <doragrestyaa@gmail.com>
2020-05-16 12:06:12 +0700

    Login Register

---
+++ lib/LoginPage.dart
@@ -0,0 +1,370 @@
+import 'package:flutter/material.dart';
+import 'package:loginregister/RegisterPage.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  var _obSecure = true;
+
+  void onHideSecure() {
+    if (_obSecure == true) {
+      setState(() {
+        _obSecure = false;
+      });
+    } else {
+      setState(() {
+        _obSecure = true;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ListView(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('images/gb.png'), fit: BoxFit.cover),
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 100, left: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Text(
+                              'Hello,',
+                              style: TextStyle(
+                                fontSize: 50,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Text(
+                              'Welcome in My Apps,',
+                              style: TextStyle(
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 70, left: 105),
+                        child: Row(
+                          children: <Widget>[
+                            Container(
+                              margin: EdgeInsets.only(left: 2, right: 2),
+                              alignment: Alignment.center,
+                              height: 35,
+                              width: 35,
+                              color: Colors.blue,
+                              child: Text('L', style: TextStyle(fontSize: 30, color: Colors.white, fontWeight: FontWeight.bold),),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(left: 2, right: 2),
+                              alignment: Alignment.center,
+                              height: 35,
+                              width: 35,
+                              color: Colors.blue,
+                              child: Text('O', style: TextStyle(fontSize: 30, color: Colors.white, fontWeight: FontWeight.bold),),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(left: 2, right: 2),
+                              alignment: Alignment.center,
+                              height: 35,
+                              width: 35,
+                              color: Colors.blue,
+                              child: Text('G', style: TextStyle(fontSize: 30, color: Colors.white, fontWeight: FontWeight.bold),),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(left: 2, right: 2),
+                              alignment: Alignment.center,
+                              height: 35,
+                              width: 35,
+                              color: Colors.blue,
+                              child: Text('I', style: TextStyle(fontSize: 30, color: Colors.white, fontWeight: FontWeight.bold),),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(left: 2, right: 2),
+                              alignment: Alignment.center,
+                              height: 35,
+                              width: 35,
+                              color: Colors.blue,
+                              child: Text('N', style: TextStyle(fontSize: 30, color: Colors.white, fontWeight: FontWeight.bold),),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                left: 50, right: 50, top: 40),
+                            child: Container(
+                              width: MediaQuery.of(context).size.width,
+                              child: Column(
+                                children: <Widget>[
+                                  TextFormField(
+                                    style: TextStyle(color: Colors.blue),
+                                    decoration: InputDecoration(
+                                        hintText: 'Email/No.Telp',
+                                        hintStyle: TextStyle(
+                                          color: Colors.blue,
+                                        ),
+                                        border: OutlineInputBorder(
+                                          borderRadius:
+                                          BorderRadius.circular(20),
+                                        )),
+                                  ),
+                                  SizedBox(
+                                    height: 15,
+                                  ),
+                                  TextFormField(
+                                    obscureText: _obSecure,
+                                    style: TextStyle(color: Colors.blue),
+                                    decoration: InputDecoration(
+                                        hintText: 'Password',
+                                        hintStyle:
+                                        TextStyle(color: Colors.blue),
+                                        suffixIcon: IconButton(
+                                          icon: Icon(
+                                            _obSecure
+                                                ? Icons.visibility
+                                                : Icons.visibility_off,
+                                            size: 35,
+                                          ),
+                                          onPressed: () {
+                                            onHideSecure();
+                                          },
+                                        ),
+                                        border: OutlineInputBorder(
+                                          borderRadius:
+                                          BorderRadius.circular(20),
+                                        )),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 160),
+                                    child: FlatButton(
+                                      child: Text('Forgot Password?', style: TextStyle(color: Colors.blue),),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 50,
+                                    width: 320,
+                                    child: RaisedButton(
+                                      onPressed: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    HomePage()));
+                                      },
+                                      color: Colors.blue,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                      ),
+                                      child: Text(
+                                        'Sign In',
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 20),
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 15),
+                                  ),
+                                  Text(
+                                    'Or',
+                                    style:
+                                    TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 10),
+                                  ),
+                                  FlatButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  RegisterPage()));
+                                    },
+                                    color: Colors.blue,
+                                    child: Text(
+                                      'Register',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 20),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 45, top: 300),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(left: 5, right: 5),
+                      alignment: Alignment.center,
+                      height: 35,
+                      width: 35,
+                      color: Colors.blue,
+                      child: Text('W', style: TextStyle(fontSize: 30),),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 5, right: 5),
+                      alignment: Alignment.center,
+                      height: 35,
+                      width: 35,
+                      color: Colors.blue,
+                      child: Text('E', style: TextStyle(fontSize: 30),),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 5, right: 5),
+                      alignment: Alignment.center,
+                      height: 35,
+                      width: 35,
+                      color: Colors.blue,
+                      child: Text('L', style: TextStyle(fontSize: 30),),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 5, right: 5),
+                      alignment: Alignment.center,
+                      height: 35,
+                      width: 35,
+                      color: Colors.blue,
+                      child: Text('C', style: TextStyle(fontSize: 30),),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 5, right: 5),
+                      alignment: Alignment.center,
+                      height: 35,
+                      width: 35,
+                      color: Colors.blue,
+                      child: Text('O', style: TextStyle(fontSize: 30),),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 5, right: 5),
+                      alignment: Alignment.center,
+                      height: 35,
+                      width: 35,
+                      color: Colors.blue,
+                      child: Text('M', style: TextStyle(fontSize: 30),),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 5, right: 5),
+                      alignment: Alignment.center,
+                      height: 35,
+                      width: 35,
+                      color: Colors.blue,
+                      child: Text('E', style: TextStyle(fontSize: 30),),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(left: 120, right: 5, top: 20),
+                      alignment: Alignment.center,
+                      height: 35,
+                      width: 35,
+                      color: Colors.blue,
+                      child: Text('I', style: TextStyle(fontSize: 30),),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 5, right: 5, top: 20),
+                      alignment: Alignment.center,
+                      height: 35,
+                      width: 35,
+                      color: Colors.blue,
+                      child: Text('N', style: TextStyle(fontSize: 30),),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(left: 80, right: 5, top: 20),
+                      alignment: Alignment.center,
+                      height: 35,
+                      width: 35,
+                      color: Colors.blue,
+                      child: Text('A', style: TextStyle(fontSize: 30),),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 5, right: 5, top: 20),
+                      alignment: Alignment.center,
+                      height: 35,
+                      width: 35,
+                      color: Colors.blue,
+                      child: Text('P', style: TextStyle(fontSize: 30),),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 5, right: 5, top: 20),
+                      alignment: Alignment.center,
+                      height: 35,
+                      width: 35,
+                      color: Colors.blue,
+                      child: Text('P', style: TextStyle(fontSize: 30),),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 5, right: 5, top: 20),
+                      alignment: Alignment.center,
+                      height: 35,
+                      width: 35,
+                      color: Colors.blue,
+                      child: Text('S', style: TextStyle(fontSize: 30),),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
---
+++ lib/RegisterPage.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+import 'package:loginregister/LoginPage.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  var _obSecure = true;
+
+  void onHideSecure() {
+    if (_obSecure == true) {
+      setState(() {
+        _obSecure = false;
+      });
+    } else {
+      setState(() {
+        _obSecure = true;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ListView(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Container(
+                  height: MediaQuery
+                      .of(context)
+                      .size
+                      .height,
+                  width: MediaQuery
+                      .of(context)
+                      .size
+                      .width,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('images/gbb.jpg'), fit: BoxFit.cover
+                      )
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 70, left: 40),
+                  child: Text('REGISTER', style: TextStyle(letterSpacing: 10,
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),),
+                ),
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          top: 150, bottom: 50, left: 20, right: 20),
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              top: 50, left: 30, right: 50, bottom: 50),
+                          child: Container(
+                            width: MediaQuery
+                                .of(context)
+                                .size
+                                .width,
+                            child: Column(
+                              children: <Widget>[
+                                TextFormField(
+                                  decoration: InputDecoration(
+                                    hintText: 'Nama Lengkap',
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 10),
+                                ),
+                                TextFormField(
+                                  decoration: InputDecoration(
+                                      hintText: 'Username'
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 10),
+                                ),
+                                TextFormField(
+                                  decoration: InputDecoration(
+                                      hintText: 'Email/No.Telp'
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 10),
+                                ),
+                                TextFormField(
+                                  obscureText: _obSecure,
+                                  decoration: InputDecoration(
+                                      hintText: 'Password',
+                                      suffixIcon: IconButton(
+                                        icon: Icon(
+                                          _obSecure ? Icons.visibility : Icons
+                                              .visibility_off, size: 25,
+                                        ),
+                                        onPressed: () {
+                                          onHideSecure();
+                                        },
+                                      )
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 10),
+                                ),
+                                TextFormField(
+                                  obscureText: _obSecure,
+                                  decoration: InputDecoration(
+                                      hintText: 'Confirm Password',
+                                      suffixIcon: IconButton(
+                                        icon: Icon(
+                                          _obSecure ? Icons.visibility : Icons
+                                              .visibility_off, size: 25,
+                                        ),
+                                        onPressed: () {
+                                          onHideSecure();
+                                        },
+                                      )
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 20),
+                                ),
+                                RaisedButton(
+                                  onPressed: () {
+                                    Navigator.push(context, MaterialPageRoute(
+                                        builder: (context) => LoginPage()));
+                                  },
+                                  color: Colors.blue,
+                                  child: Text('Sign Up', style: TextStyle(
+                                      color: Colors.white, fontSize: 20),),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
